d32bbabb12ac3e61adf2ab57ba95d39f0c2cb72f
Hoon <varick137@gmail.com>
2018-07-17 19:29:15 +0900

    버튼아래에 맵뜨게함

--- Practice/app/src/main/java/com/example/taehun/practice/MainActivity.java
+++ Practice/app/src/main/java/com/example/taehun/practice/MainActivity.java
@@ -5,18 +5,43 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.os.Bundle;
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NMapActivity {
 
     Button button1;
     Button button2;
+
+    private LinearLayout mapLayout;
+    //private ViewGroup mapLayout;
+    private NMapView mMapView;
+    private final String CLIENT_ID = "nhGQ8oQrA0kBxYqNyUz8";// 애플리케이션 클라이언트 아이디 값
+
+    private void init(){
+        mMapView = new NMapView(this);
+        mMapView.setClientId(CLIENT_ID); // 클라이언트 아이디 값 설정
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.setScalingFactor(1.5f);
+        mMapView.requestFocus();
+
+        mapLayout = findViewById(R.id.map);
+        mapLayout.addView(mMapView);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
 
         button1 = (Button)findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
